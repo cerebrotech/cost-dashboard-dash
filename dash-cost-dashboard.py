3a618021e3be638854d6b4a7c9497d02523a4aa8
Sebastian Rodriguez <sebastian.rodriguez@dominodatalab.com>
2024-05-03 18:18:22 -0300

    Use 7d option for 7 days retrieval

--- dash-cost-dashboard.py
+++ dash-cost-dashboard.py
@@ -50,7 +50,7 @@ NO_TAG = "No tag"
 window_to_param = {
     "30d": "Last 30 days",
     "14d": "Last 14 days",
-    "lastweek": "Last 7 days"
+    "7d": "Last 7 days"
 }
 
 def get_today_timestamp():
